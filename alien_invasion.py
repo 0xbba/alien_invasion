38993dea7ef7baf21e230d867e8ef50ab8d7970e
cachorro <tendoyor@gmail.com>
2022-08-30 16:35:25 +0800

    添加一个Settings类 用于配置窗口的尺寸和背景颜色

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,15 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         """初始化游戏并创建游戏资源。"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (211,211,211)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -18,7 +19,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #每次循环时都重绘屏幕。
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             #让最近绘制的屏幕可见。
             pygame.display.flip()
 if __name__ == '__main__':
